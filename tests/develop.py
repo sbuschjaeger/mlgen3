c62e827ea15f160e296d805d41b12253b9f740a8
Leonard David <leonard.bereholschi@tu-dortmund.de>
2025-07-29 15:24:30 +0200

    add missing init files and update pythonpath

--- tests/develop.py
+++ tests/develop.py
@@ -1,5 +1,13 @@
 #!/usr/bin/env python3
 
+from pathlib import Path
+import sys
+
+path_root = Path(__file__).parents[1]
+sys.path.append(str(path_root))
+
+# print(sys.path)
+
 from test_nn import TestNeuralNetwork 
 
 test = TestNeuralNetwork()
